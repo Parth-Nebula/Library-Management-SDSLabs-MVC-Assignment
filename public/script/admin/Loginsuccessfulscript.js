406b8efcdf4ecf8b7f03e64000e54b8444bcb89d
Parth-Nebula <99603309+Parth-Nebula@users.noreply.github.com>
2022-07-19 01:12:27 +0530

    Update Loginsuccessfulscript.js

--- public/script/admin/Loginsuccessfulscript.js
+++ public/script/admin/Loginsuccessfulscript.js
@@ -1,3 +1,2 @@
 document.getElementById("welcome").innerHTML = "Welcome " + data_one + " !" ;
 sessionStorage.setItem("username", data_one);
-document.getElementById("username").defaultValue = data_one;
