4b72d4eaa4fa20db3ddf80e968810d84303c9abb
Parth-Nebula <parthmittal.nebula@gmail.com>
2022-07-21 16:04:53 +0530

    removing deny request

--- app/controllers/admin/actonarequest.php
+++ app/controllers/admin/actonarequest.php
@@ -17,26 +17,11 @@ class ActOnARequest
             $request = \Model\Admin\IssueRequests::client_book_all( $_POST["ClientName"] , $_POST["BookTitle"] ) ;
             if ( $request )
             {
-                if ( $_POST["Action"] == 1 )
+                $books_available = \Model\Admin\Books::book_all( $_POST["BookTitle"] ) ;
+                if ( ( $request["Status"] == 2 or $request["Status"] == 0 ) and ($books_available["QuantityAvailable"] != 0 ) )
                 {
-                    $books_available = \Model\Admin\Books::book_all( $_POST["BookTitle"] ) ;
-                    if ( ( $request["Status"] == 2 or $request["Status"] == 0 ) and ($books_available["QuantityAvailable"] != 0 ) )
-                    {
-                        \Model\Admin\Books::book_update_quantity_available_minus_one( $_POST["BookTitle"] ) ;
-                        \Model\Admin\IssueRequests::client_book_update_status_one ( $_POST["ClientName"] , $_POST["BookTitle"] ) ;
-                    }
-                }
-                else if ( $_POST["Action"] == 2)
-                {
-                    if ( $request["Status"] == 1 )
-                    {
-                        \Model\Admin\Books::book_update_quantity_available_plus_one( $_POST["BookTitle"] ) ;
-                        \Model\Admin\IssueRequests::client_book_update_status_two ( $_POST["ClientName"] , $_POST["BookTitle"] ) ; 
-                    }
-                    if ( $request["Status"] == 0 )
-                    {
-                        \Model\Admin\IssueRequests::client_book_update_status_two ( $_POST["ClientName"] , $_POST["BookTitle"] ) ;
-                    }
+                    \Model\Admin\Books::book_update_quantity_available_minus_one( $_POST["BookTitle"] ) ;
+                    \Model\Admin\IssueRequests::client_book_update_status_one ( $_POST["ClientName"] , $_POST["BookTitle"] ) ;
                 }
             }
             $requested_books_with_amount = \Model\Admin\Joins::issue_requests_books();
--- public/script/admin/Issuesrequestscript.js
+++ public/script/admin/Issuesrequestscript.js
@@ -6,7 +6,6 @@ let cell2;
 let cell3;
 let cell4;
 let cell5;
-let cell6;
 row = table.insertRow(0);
 cell1 = row.insertCell(0);
 cell1.style = "padding: 1vw; width: 27vw; font-size: 2.4vw;";
@@ -18,8 +17,6 @@ cell4 = row.insertCell(3);
 cell4.style = "padding: 1vw; width: 27vw; font-size: 2.4vw;";
 cell5 = row.insertCell(4);
 cell5.style = "padding: 1vw; width: 27vw; font-size: 2.4vw;";
-cell6 = row.insertCell(5);
-cell6.style = "padding: 1vw; width: 27vw; font-size: 2.4vw;";
 cell1.innerHTML = "Book";
 cell2.innerHTML = "Available" ;
 cell3.innerHTML = "User";
@@ -37,29 +34,29 @@ for (let i = 0 ; i < list_of_requests.length ; i++)
     cell4.style = "padding: 1vw; width: 27vw;" ;
     cell5 = row.insertCell(4);
     cell5.style = "padding: 1vw; width: 27vw;" ;
-    cell6 = row.insertCell(5);
-    cell6.style = "padding: 1vw; width: 27vw;" ;
     cell1.innerHTML = list_of_requests[i].Title;
     cell2.innerHTML = list_of_requests[i].QuantityAvailable;
+    if ( ! (list_of_requests[i].QuantityAvailable) )
+    {
+        cell2.innerHTML = 0 ;        
+    }
     cell3.innerHTML = list_of_requests[i].Username;
     javaDate = new Date(list_of_requests[i].RequestDate);
     cell4.innerHTML = String(javaDate.getFullYear()) + "-" + String(javaDate.getMonth()+1) + "-" + String(javaDate.getDate()) ;
     if (list_of_requests[i].Status == 0)
         {
-            if (list_of_requests[i].QuantitAvailable == 0)
+            if (list_of_requests[i].QuantityAvailable == 0  ||  !( list_of_requests[i].QuantityAvailable ) ) 
             {
                 cell5.innerHTML = "Unavailable" ;
             }
             else
             {
-                cell5.innerHTML = " <form action='/actOnARequest' method='POST'> <input type='text' class='bookTitle' name='BookTitle' value='" + list_of_requests[i].Title + "' style='display:none'> <input type='text' class='clientName' name='ClientName' value='"+ list_of_requests[i].Username + "' + style='display:none'> <input type='text' class='action' name='Action' value=1 style='display:none'> <input type='submit' class='actButton' value='Accept'> </form> " ;
+                cell5.innerHTML = " <form action='/actOnARequest' method='POST'> <input type='text' class='bookTitle' name='BookTitle' value='" + list_of_requests[i].Title + "' style='display:none'> <input type='text' class='clientName' name='ClientName' value='"+ list_of_requests[i].Username + "' + style='display:none'> <input type='submit' class='actButton' value='Accept'> </form> " ;
             }
-            cell6.innerHTML = " <form action='/actOnARequest' method='POST'> <input type='text' class='bookTitle' name='BookTitle' value='" + list_of_requests[i].Title + "' style='display:none'> <input type='text' class='clientName' name='ClientName' value='"+ list_of_requests[i].Username + "' + style='display:none'> <input type='text' class='action' name='Action' value=2 style='display:none'> <input type='submit' class='actButton' value='Deny'> </form> " ;
         }
     else if (list_of_requests[i].Status == 1)
         {
             cell5.innerHTML = "Accepted";
-            cell6.innerHTML = " <form action='/actOnARequest' method='POST'> <input type='text' class='bookTitle' name='BookTitle' value='" + list_of_requests[i].Title + "' style='display:none'> <input type='text' class='clientName' name='ClientName' value='"+ list_of_requests[i].Username + "' + style='display:none'> <input type='text' class='action' name='Action' value=2 style='display:none'> <input type='submit' class='actButton' value='Deny'> </form> " ;
         }
     else
         {
@@ -69,8 +66,7 @@ for (let i = 0 ; i < list_of_requests.length ; i++)
             }
             else
             {
-                cell5.innerHTML = " <form action='/actOnARequest' method='POST'> <input type='text' class='bookTitle' name='BookTitle' value='" + list_of_requests[i].Title + "' style='display:none'> <input type='text' class='clientName' name='ClientName' value='"+ list_of_requests[i].Username + "' + style='display:none'> <input type='text' class='action' name='Action' value=1 style='display:none'> <input type='submit' class='actButton' value='Accept'> </form> " ;
+                cell5.innerHTML = " <form action='/actOnARequest' method='POST'> <input type='text' class='bookTitle' name='BookTitle' value='" + list_of_requests[i].Title + "' style='display:none'> <input type='text' class='clientName' name='ClientName' value='"+ list_of_requests[i].Username + "' + style='display:none'> <input type='submit' class='actButton' value='Accept'> </form> " ;
             }
-            cell6.innerHTML = "Denied" ;
         }
 }
